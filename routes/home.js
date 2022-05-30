a600c5210a5e69b8d75ae651df44fdfecbab63f7
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-05-30 14:50:45 +0100

    added name to inputs

--- routes/home.js
+++ routes/home.js
@@ -10,24 +10,24 @@ function get(request, response) {
   <h1>${title}</h1>
   <form action="/post" method="post" enctype="multipart/form-data">
   <label for="product-name"> Product Name </label>
-  <input type="text" id="product-name" />
+  <input type="text" id="product-name" name="item_name" />
 
   <label for="product-price"> Product Price </label>
-  <input type="number" id="product-price" />
+  <input type="number" id="product-price" name ="item_price"/>
 
-  <label for="product-price"> Product Price </label>
-  <input type="number" id="product-price" />
+
 
   <label for="product-discrption"> Product discrption </label>
   <textarea
-    name="product-discrption"
+    name="item_info"
     id="product-discrption"
     cols="30"
     rows="10"
+
   ></textarea>
 
   <label for="uploaded-file"> upload Image </label>
-  <input type="file" id="uploaded-file" name="uploaded_file"/>
+  <input type="file" id="uploaded-file" name="item_image"/>
 
   <button type="submit">submit</button>
 </form>`;

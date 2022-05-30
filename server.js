7d537170a9cb5bbecc0146a6c7183471f3455c1e
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-05-30 10:33:13 +0100

    setup

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
